3506bca6d9544295b4b1107fbb28872d597ecb56
PaulHuaman696 <anderson.huaman@unas.edu.pe>
2025-04-24 10:41:30 -0500

    rutas relativas

--- public/script.js
+++ public/script.js
@@ -24,10 +24,8 @@ contactForm.addEventListener("submit", async (e) => {
     ciudad: inputs[2].value,
   };
 
-  const ip_host = await getAPIHost();
-
   try {
-    const res = await fetch(`https://${ip_host}:3001/api/contact`, {
+    const res = await fetch("/api/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
